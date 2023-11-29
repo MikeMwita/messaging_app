0b4e1e0222981c1dd6a1d1824d228464b9def2dd
MikeMwita <mwitamike8@gmail.com>
2023-11-29 16:06:40 +0300

    defines routes

--- internal/delivery/htttp/routes.go
+++ internal/delivery/htttp/routes.go
@@ -1,1 +1,18 @@
 package htttp
+
+import (
+	"github.com/MikeMwita/messaging_app.git/internal/ports"
+	"github.com/gin-gonic/gin"
+)
+
+// SetRoutes sets up the API routes
+
+func SetRoutes(r *gin.Engine, repo ports.Repository) {
+	messageHandler := NewMessageHandler(repo)
+
+	r.GET("/messages", messageHandler.GetMessagesHandler)
+	r.GET("/messages/:id", messageHandler.GetMessageHandler)
+	r.POST("/messages", messageHandler.SendMessageHandler)
+	r.POST("/simulate/send-message", messageHandler.SimulateSendMessageHandler)
+	r.GET("/simulate/receive-messages", messageHandler.SimulateReceiveMessagesHandler)
+}
