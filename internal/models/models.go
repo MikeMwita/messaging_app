2df7c4dea1c3f09c68a273285394098af16933fa
MikeMwita <mwitamike8@gmail.com>
2023-11-29 16:05:10 +0300

    define the models data structure

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,15 @@
 package models
+
+type Message struct {
+	ID      int    `json:"id"`
+	UserID  int    `json:"user_id"`
+	Time    string `json:"time"`
+	Content string `json:"content"`
+}
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Role  string `json:"role"`
+	Image string `json:"image"`
+}
