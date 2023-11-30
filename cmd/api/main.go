9ba161fb35333094f067823bae5606cc3f7713d4
MikeMwita <mwitamike8@gmail.com>
2023-11-30 14:45:57 +0300

    adds db instansiation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,3 @@
-// cmd/api/main.go
-
 package main
 
 import (
@@ -8,18 +6,31 @@ import (
 	"github.com/MikeMwita/messaging_app.git/internal/delivery/htttp"
 	"github.com/MikeMwita/messaging_app.git/internal/repository"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
 
 func main() {
+	password := "1A5hhh3qsjQQdUA6IajljFTXoDQKEcwo"
 	dbConnectionString := os.Getenv("DB_CONNECTION_STRING")
+
+	if dbConnectionString == "" {
+		dbConnectionString = fmt.Sprintf("postgres://mike:%s@dpg-clk5aoeg1b2c739gus30-a.oregon-postgres.render.com/mike_dy9f?sslmode=require", password)
+	}
+
 	db, err := sql.Open("postgres", dbConnectionString)
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
+	defer db.Close()
 
+	// Create a repository instance
 	repo := repository.NewRepository(db)
+	err = repo.CreateTable()
+	if err != nil {
+		log.Fatal("Failed to create table:", err)
+	}
 
 	// Seed the database with messages from CSV
 	csvFilePath := "/home/mike/Downloads/GeneralistRails_Project_MessageData.csv"
@@ -29,15 +40,15 @@ func main() {
 	}
 
 	router := gin.Default()
+
 	htttp.SetRoutes(router, repo)
 
-	// Start the server
 	portStr := os.Getenv("PORT")
 	if portStr == "" {
 		portStr = "8080"
 	}
-
-	serverAddr := fmt.Sprintf(":%d", portStr)
-	fmt.Printf("Server is running on http://localhost:%d\n", portStr)
+	// Starting  the server
+	serverAddr := fmt.Sprintf(":%s", portStr)
+	fmt.Printf("Server is running on http://localhost:%s\n", portStr)
 	log.Fatal(router.Run(serverAddr))
 }
