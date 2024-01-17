3e4d4cc125aa7eb44ed89a1f01d6dc1b91652f8c
MikeMwita <114430148+MikeMwita@users.noreply.github.com>
2024-01-17 10:00:04 +0300

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,45 +25,4 @@ func main() {
 	log.Fatal(router.Run(serverAddr))
 }
 
-//
-//func main() {
-//	password := os.Getenv("DB_PASSWORD")
-//	dbConnectionString := os.Getenv("DB_CONNECTION_STRING")
-//
-//	if dbConnectionString == "" {
-//		dbConnectionString = fmt.Sprintf("postgres://mike:%s@dpg-clk5aoeg1b2c739gus30-a.oregon-postgres.render.com/mike_dy9f?sslmode=require", password)
-//	}
-//
-//	db, err := sql.Open("postgres", dbConnectionString)
-//	if err != nil {
-//		log.Fatal("Failed to connect to the database:", err)
-//	}
-//	defer db.Close()
-//
-//	// Create a repository instance
-//	repo := repository.NewMessageRepository(db)
-//	err = repo.CreateTable()
-//	if err != nil {
-//		log.Fatal("Failed to create table:", err)
-//	}
-//
-//	// Seed the database with messages from CSV
-//	csvFilePath := "GeneralistRails_Project_MessageData.csv"
-//	err = repo.SeedMessagesFromCSV(csvFilePath)
-//	if err != nil {
-//		log.Fatal("Failed to seed the database:", err)
-//	}
-//
-//	router := gin.Default()
-//
-//	htttp.SetRoutes(router, repo)
-//
-//	portStr := os.Getenv("PORT")
-//	if portStr == "" {
-//		portStr = "8080"
-//	}
-//	// Starting  the server
-//	serverAddr := fmt.Sprintf(":%s", portStr)
-//	fmt.Printf("Server is running on http://localhost:%s\n", portStr)
-//	log.Fatal(router.Run(serverAddr))
-//}
+
